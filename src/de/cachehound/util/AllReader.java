1dcf177c8cd8b27560d3cf94685bc62ba03d5f46
google <google@uncleowen.de>
2009-07-28 21:55:36 +0000

    Source: Added junit to lib/ and .classpath Added AllReader (decorator to Reader, that has a readAll() method) + TestCase

---
+++ src/de/cachehound/util/AllReader.java
@@ -0,0 +1,35 @@
+package de.cachehound.util;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class AllReader extends Reader {
+	private Reader r;
+	private int bufSize = 1024;
+
+	public AllReader(Reader r) {
+		this.r = r;
+	}
+
+	@Override
+	public void close() throws IOException {
+		r.close();
+	}
+
+	@Override
+	public int read(char[] cbuf, int off, int len) throws IOException {
+		return r.read(cbuf, off, len);
+	}
+
+	public String readAll() throws IOException {
+		char[] buffer = new char[bufSize];
+		StringBuilder sb = new StringBuilder();
+		int bytesRead;
+		
+		while ((bytesRead = r.read(buffer)) != -1) {
+			sb.append(buffer, 0, bytesRead);
+		}
+		
+		return sb.toString();
+	}
+}
