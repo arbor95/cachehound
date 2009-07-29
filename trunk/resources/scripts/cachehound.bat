9ce2ba5be7a72263013932646ec95ddb16e55c1f
tweetyHH <tweetyHH@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-07-29 21:19:29 +0000

    Source: Logging Hinzugefügt: SLF4J und als Backend Logback. Bisher nur eine Test Loggingsausgabe im EmailReader, aber kommt Zeit, kommt logging ;-)

--- trunk/resources/scripts/cachehound.bat
+++ trunk/resources/scripts/cachehound.bat
@@ -1,1 +1,1 @@
-java -cp CacheHound.jar:ewe.jar:someClasses.jar:mail.jar ewe.applet.Applet CacheWolf.CacheWolf
+java -Dlogback.configurationFile=config\logback.dist.xml -cp CacheHound.jar:ewe.jar:someClasses.jar:mail.jar:slf4j-api-1.5.8.jar:slf4j-ext-1.5.8.jar:logback-classic-0.9.16.jar:logback-core-0.9.16.jar ewe.applet.Applet CacheWolf.CacheWolf
