7aff487fd4e6989b15a1a842bfa0e4bb8b8944ba
tweetyHH <tweetyHH@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-07-31 22:28:10 +0000

    Source: StartSkripte an die Änderungen von UncleOwen angepasst.

--- trunk/resources/scripts/cachehound.sh
+++ trunk/resources/scripts/cachehound.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dlogback.configurationFile=config/logback.dist.xml -cp CacheHound.jar:ewe.jar:someClasses.jar:mail.jar:slf4j-api-1.5.8.jar:slf4j-ext-1.5.8.jar:logback-classic-0.9.16.jar:logback-core-0.9.16.jar ewe.applet.Applet CacheWolf.CacheWolf
+java -Dlogback.configurationFile=config/logback.dist.xml -cp CacheHound.jar:ewe.jar:ewe_misc.jar:mail.jar:slf4j-api-1.5.8.jar:slf4j-ext-1.5.8.jar:logback-classic-0.9.16.jar:logback-core-0.9.16.jar ewe.applet.Applet CacheWolf.CacheWolf
