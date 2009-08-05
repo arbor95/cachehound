718d71ba31e6f28e53ad09efe010efe17724646d
google <google@uncleowen.de>
2009-08-05 19:58:07 +0000

    Source: ListHelper.divideAndSwap eingefuerhrt und damit FilterData.getFilterRoseAsString() umformuliert

---
+++ src/de/cachehound/util/ListHelper.java
@@ -0,0 +1,22 @@
+package de.cachehound.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListHelper {
+	/**
+	 * Die uebergebene Liste wird in 2 Teile geteilt und eine neue Liste
+	 * zurueckgegeben, in der der zweite Teil vor dem ersten ist. Die Trennung
+	 * erfolgt am uebergebenen Element. Genauer: Das erste Vorkommen von elem
+	 * ist das erste Element der neuen Liste.
+	 */
+	public static <E> List<E> divideAndSwap(List<E> l, E elem) {
+		int index = l.indexOf(elem);
+
+		List<E> ret = new ArrayList<E>();
+		ret.addAll(l.subList(index, l.size()));
+		ret.addAll(l.subList(0, index));
+
+		return ret;
+	}
+}
---
+++ src/de/cachehound/util/ListHelperTest.java
@@ -0,0 +1,14 @@
+package de.cachehound.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class ListHelperTest {
+	@Test
+	public void testDivideAndSwap() {
+		assertEquals(Arrays.asList(3,4,1,2), ListHelper.divideAndSwap(Arrays.asList(1,2,3,4), 3));
+	}
+}
