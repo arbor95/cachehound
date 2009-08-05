fa8c27bb92563e658caf233adedce461575522ab
google <google@uncleowen.de>
2009-08-05 20:49:46 +0000

    Source: BearingComparator: Sorting alphabetically doesn't make much sense.

--- src/de/cachehound/comparators/BearingComparator.java
+++ src/de/cachehound/comparators/BearingComparator.java
@@ -7,7 +7,6 @@ import CacheWolf.beans.CacheHolder;
 public class BearingComparator implements Comparator<CacheHolder> {
 	@Override
 	public int compare(CacheHolder o1, CacheHolder o2) {
-		//FIXME: is this really the way bearings should be compared?
-		return o1.getBearingAsString().compareTo(o2.getBearingAsString());
+		return o1.getBearing().compareTo(o2.getBearing());
 	}
 }
