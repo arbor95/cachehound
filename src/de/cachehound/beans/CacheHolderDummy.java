589cc4793c6c24685f645b33b5f46898f07a6e96
google <google@uncleowen.de>
2009-08-06 18:11:27 +0000

    Source: CacheHolder implementiert jetzt ein ICacheHolder (kann gerne noch erweitert werden)

---
+++ src/de/cachehound/beans/CacheHolderDummy.java
@@ -0,0 +1,25 @@
+package de.cachehound.beans;
+
+import de.cachehound.types.Bearing;
+import de.cachehound.types.CacheSize;
+
+/**
+ * A dummy, non-working implementation of ICacheHolder.
+ * It is meant as a base class for mock objects.
+ */
+public class CacheHolderDummy implements ICacheHolder {
+	@Override
+	public Bearing getBearing() {
+		return null;
+	}
+
+	@Override
+	public CacheSize getCacheSize() {
+		return null;
+	}
+	
+	@Override
+	public double getKilom() {
+		return Double.NaN;
+	}
+}
---
+++ src/de/cachehound/beans/ICacheHolder.java
@@ -0,0 +1,10 @@
+package de.cachehound.beans;
+
+import de.cachehound.types.Bearing;
+import de.cachehound.types.CacheSize;
+
+public interface ICacheHolder {
+	public Bearing getBearing();
+	public CacheSize getCacheSize();
+	public double getKilom();
+}
