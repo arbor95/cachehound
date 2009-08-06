2b2d9e9dc3e77d77b9f815ee914e3160a060d9c3
google <google@uncleowen.de>
2009-08-06 18:13:28 +0000

    Source: und erste Ansaetze einer GUI fuer die neuen Filter.

---
+++ src/de/cachehound/gui/filter/FilterEditor.java
@@ -0,0 +1,125 @@
+package de.cachehound.gui.filter;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import de.cachehound.filter.IFilter;
+
+public class FilterEditor extends JDialog {
+	/** A return status code - returned if Cancel button has been pressed */
+	public static final int RET_CANCEL = 0;
+	/** A return status code - returned if OK button has been pressed */
+	public static final int RET_OK = 1;
+
+	public FilterEditor(IFilter old, JFrame parent, boolean modal) {
+		super(parent, modal);
+		initComponents();
+		// setState(old);
+	}
+
+	/** @return the return status of this dialog - one of RET_OK or RET_CANCEL */
+	public int getReturnStatus() {
+		return returnStatus;
+	}
+
+	/**
+	 * This method is called from within the constructor to initialize the form.
+	 */
+	private void initComponents() {
+		JPanel treePanel = createTreePanel();
+		JPanel buttonPanel = createButtonPanel();
+
+		this.setLayout(new BorderLayout());
+		this.add(treePanel, BorderLayout.CENTER);
+		this.add(buttonPanel, BorderLayout.SOUTH);
+
+		pack();
+	}
+
+	private JPanel createTreePanel() {
+		// TODO Auto-generated method stub
+		return new JPanel();
+	}
+
+	private JPanel createButtonPanel() {
+		JPanel buttonPanel = new JPanel();
+
+		okButton = new JButton();
+		cancelButton = new JButton();
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				closeDialog(evt);
+			}
+		});
+
+		okButton.setText("OK");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				okButtonActionPerformed(evt);
+			}
+		});
+
+		cancelButton.setText("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				cancelButtonActionPerformed(evt);
+			}
+		});
+
+		buttonPanel.add(okButton);
+		buttonPanel.add(cancelButton);
+
+		return buttonPanel;
+	}
+
+	private void okButtonActionPerformed(ActionEvent evt) {
+		doClose(RET_OK);
+	}
+
+	private void cancelButtonActionPerformed(ActionEvent evt) {
+		doClose(RET_CANCEL);
+	}
+
+	/** Closes the dialog */
+	private void closeDialog(WindowEvent evt) {
+		doClose(RET_CANCEL);
+	}
+
+	private void doClose(int retStatus) {
+		returnStatus = retStatus;
+		setVisible(false);
+		dispose();
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				FilterEditor dialog = new FilterEditor(null, new JFrame(), true);
+				dialog.addWindowListener(new WindowAdapter() {
+					public void windowClosing(WindowEvent e) {
+						System.exit(0);
+					}
+				});
+				dialog.setVisible(true);
+			}
+		});
+	}
+
+	private JButton cancelButton;
+	private JButton okButton;
+
+	private int returnStatus = RET_CANCEL;
+}
---
+++ src/de/cachehound/gui/filter/SizeFilterEditor.java
@@ -0,0 +1,76 @@
+package de.cachehound.gui.filter;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import de.cachehound.filter.SizeFilter;
+import de.cachehound.types.CacheSize;
+
+public class SizeFilterEditor extends AbstractSimpleFilterEditor<SizeFilter> {
+	/** Creates new form CacheSizeFilterEditor */
+	public SizeFilterEditor(SizeFilter old, JFrame parent, boolean modal) {
+		super(old, parent, modal);
+	}
+
+	public SizeFilter getFilter() {
+		Set<CacheSize> mask = EnumSet.noneOf(CacheSize.class);
+		
+		for (CacheSize b : CacheSize.values()) {
+			if (boxes.get(b).isSelected()) {
+				mask.add(b);
+			}
+		}
+		
+		return new SizeFilter(mask);
+	}
+
+	protected void setState(SizeFilter old) {
+		if (old != null) {
+			for (CacheSize b : old.getMask()) {
+				boxes.get(b).setSelected(true);
+			}
+		}
+	}
+
+	protected JPanel createCheckBoxesPanel() {
+		JPanel panel = new JPanel();
+		boxes = new EnumMap<CacheSize, JCheckBox>(CacheSize.class);
+	
+		for (CacheSize b : CacheSize.values()) {
+			JCheckBox box = new JCheckBox(b.toString());
+			panel.add(box);
+			boxes.put(b, box);
+		}
+	
+		return panel;
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				SizeFilterEditor dialog = new SizeFilterEditor(null,
+						new JFrame(), true);
+				dialog.addWindowListener(new WindowAdapter() {
+					public void windowClosing(WindowEvent e) {
+						System.exit(0);
+					}
+				});
+				dialog.setVisible(true);
+			}
+		});
+	}
+
+	private Map<CacheSize, JCheckBox> boxes;
+}
