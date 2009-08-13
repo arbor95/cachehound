6005c4b563558e9ebc23dc149d200296cb353301
google@uncleowen.de <google@uncleowen.de@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-08-13 19:16:06 +0000

    Source: equals und hashcode bei den filtern implementiert

--- trunk/src/de/cachehound/filter/DistanceFilter.java
+++ trunk/src/de/cachehound/filter/DistanceFilter.java
@@ -22,4 +22,29 @@ public class DistanceFilter extends SimpleFilter {
 	public String toString() {
 		return "Cache is nearer than " + limit + "km.";
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(limit);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DistanceFilter other = (DistanceFilter) obj;
+		if (Double.doubleToLongBits(limit) != Double
+				.doubleToLongBits(other.limit))
+			return false;
+		return true;
+	}
 }
