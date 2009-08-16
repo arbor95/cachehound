ebf13ea2db5116771d78baaccba169feede8c838
google@uncleowen.de <google@uncleowen.de@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-08-16 17:12:22 +0000

    Source: +DifficultyFilterPanel, +TerrainFilterPanel

--- trunk/src/de/cachehound/gui/filter/FilterTabbedPane.java
+++ trunk/src/de/cachehound/gui/filter/FilterTabbedPane.java
@@ -6,13 +6,17 @@ import javax.swing.JTabbedPane;
 
 import de.cachehound.filter.IFilter;
 import de.cachehound.gui.filter.panels.BearingFilterPanel;
+import de.cachehound.gui.filter.panels.DifficultyFilterPanel;
 import de.cachehound.gui.filter.panels.DistanceFilterPanel;
 import de.cachehound.gui.filter.panels.DummyFilterPanel;
 import de.cachehound.gui.filter.panels.IFilterEditor;
 import de.cachehound.gui.filter.panels.SizeFilterPanel;
+import de.cachehound.gui.filter.panels.TerrainFilterPanel;
 
 public class FilterTabbedPane extends JTabbedPane {
 	public FilterTabbedPane() {
+		addTab("Difficulty", new DifficultyFilterPanel());
+		addTab("Terrain", new TerrainFilterPanel());
 		addTab("Bearing", new BearingFilterPanel());
 		addTab("Distance", new DistanceFilterPanel());
 		addTab("Size", new SizeFilterPanel());
