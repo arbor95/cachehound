810814006ac471116182a96b91b501b7fa8a6c88
google@uncleowen.de <google@uncleowen.de@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-09-21 15:57:46 +0000

    source:txw2.jar hinzugefügt

--- trunk/resources/scripts/cachehound.bat
+++ trunk/resources/scripts/cachehound.bat
@@ -1,1 +1,1 @@
-java -Dlogback.configurationFile=config\logback.dist.xml -cp CacheHound.jar;ewe.jar;ewe_misc.jar;mail.jar;slf4j-api-1.5.8.jar;slf4j-ext-1.5.8.jar;logback-classic-0.9.16.jar;logback-core-0.9.16.jar;TableLayout.jar;openmap.jar ewe.applet.Applet CacheWolf.CacheWolf
+java -Dlogback.configurationFile=config\logback.dist.xml -cp CacheHound.jar;ewe.jar;ewe_misc.jar;mail.jar;slf4j-api-1.5.8.jar;slf4j-ext-1.5.8.jar;logback-classic-0.9.16.jar;logback-core-0.9.16.jar;TableLayout.jar;openmap.jar;twx2.jar ewe.applet.Applet CacheWolf.CacheWolf
