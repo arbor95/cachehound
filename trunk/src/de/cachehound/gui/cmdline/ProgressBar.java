3ebceb8ef9278fd9c6773050594322928029d564
google@uncleowen.de <google@uncleowen.de@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-09-21 14:06:15 +0000

    Source: ProgressBar und IProgressBar hinzugefügt

---
+++ trunk/src/de/cachehound/gui/cmdline/ProgressBar.java
@@ -0,0 +1,24 @@
+/**
+ * Kommandozeilenversion der Fortschrittsanzeige. Zeigt den Fortschritt in
+ * Zeilen wie folgender an:
+ * 
+ * (17/42) Blahfasel
+ */
+
+package de.cachehound.gui.cmdline;
+
+import de.cachehound.gui.interfaces.IProgressBar;
+
+public class ProgressBar implements IProgressBar {
+	public ProgressBar(int max) {
+		this.max = max;
+		this.current = 0;
+	}
+
+	@Override
+	public void tick(String status) {
+		System.out.println(String.format("(%i/%i) %s", ++current, max, status));
+	}
+
+	private int max, current;
+}
