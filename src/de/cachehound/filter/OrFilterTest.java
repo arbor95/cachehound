4487012b483d6e0586f8fad82196e412a038ab8b
google <google@uncleowen.de>
2009-10-03 15:32:58 +0000

    source: Imports aufgeräumt

--- src/de/cachehound/filter/OrFilterTest.java
+++ src/de/cachehound/filter/OrFilterTest.java
@@ -3,7 +3,6 @@ package de.cachehound.filter;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class OrFilterTest {
