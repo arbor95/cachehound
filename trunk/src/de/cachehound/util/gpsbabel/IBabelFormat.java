e701f90bec5c48ae204f98403e7c9a880a9bbbe0
google@uncleowen.de <google@uncleowen.de@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-10-04 16:09:22 +0000

    source: IBabelFormat kommentiert

--- trunk/src/de/cachehound/util/gpsbabel/IBabelFormat.java
+++ trunk/src/de/cachehound/util/gpsbabel/IBabelFormat.java
@@ -1,6 +1,28 @@
 package de.cachehound.util.gpsbabel;
 
+/**
+ * Laut {@code man gpsbabel} sieht ein Aufruf von gpsbabel folgendermassen aus:
+ * <pre>
+ * /usr/bin/gpsbabel [options] -i INTYPE -f INFILE -o OUTTYPE -F OUTFILE
+ * /usr/bin/gpsbabel [options] -i INTYPE -o OUTTYPE INFILE [OUTFILE]
+ * </pre>
+ * Diese Klasse kapselt ein Paar aus INTYPE, INFILE (bzw. OUTTYPE, OUTFILE). Der
+ * Type kann dabei noch optionale Optionen enthalten. Implementierungen dieser
+ * Klasse sollten für jeden Parameter einen eigenen Setter anbieten. {@code
+ * getType} liefert alles zusammen im von gpsbabel erwarteten Format zurück.
+ * 
+ * @author uo
+ * 
+ */
 public interface IBabelFormat {
+	/**
+	 * Liefert den Typ zusammen mit den Optionen als Komma-separierten String
+	 * zurück.
+	 */
 	public String getType();
+
+	/**
+	 * Der Datei- oder Devicename, von dem gelesen bzw. in den geschrieben wird.
+	 */
 	public String getFileName();
 }
