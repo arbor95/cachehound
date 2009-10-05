b4f41e6208e95c48ec36f334e7d0af0ad60cadbb
google@uncleowen.de <google@uncleowen.de@2ee067f6-77a4-11de-a94e-f9a6307510a4>
2009-10-05 10:16:26 +0000

    source: Wo wir jetzt 'ne eigene ROT13-Klasse haben, hat die natürlich auch unit-Tests verdient. Und wenn wir schonmal unit-Tests haben, kann ich den Code auch gleich so umschreiben, dass er mir gefällt :)

---
+++ trunk/src/de/cachehound/util/Rot13Test.java
@@ -0,0 +1,47 @@
+package de.cachehound.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Rot13Test {
+	@Test
+	public void testSingleLowercaseChars() {
+		assertEquals('n', Rot13.encodeRot13('a'));
+		assertEquals('z', Rot13.encodeRot13('m'));
+		assertEquals('a', Rot13.encodeRot13('n'));
+		assertEquals('m', Rot13.encodeRot13('z'));
+	}
+
+	@Test
+	public void testSingleUppercaseChars() {
+		assertEquals('N', Rot13.encodeRot13('A'));
+		assertEquals('Z', Rot13.encodeRot13('M'));
+		assertEquals('A', Rot13.encodeRot13('N'));
+		assertEquals('M', Rot13.encodeRot13('Z'));
+	}
+
+	@Test
+	public void testSingleSpecialChars() {
+		assertEquals(' ', Rot13.encodeRot13(' '));
+		assertEquals('.', Rot13.encodeRot13('.'));
+		assertEquals('[', Rot13.encodeRot13('['));
+		assertEquals(']', Rot13.encodeRot13(']'));
+	}
+	
+	@Test
+	public void testBaum() {
+		assertEquals("Onhz", Rot13.encodeRot13("Baum"));
+	}
+	
+	@Test
+	public void testMagnetisch() {
+		assertEquals("zntargvfpu", Rot13.encodeRot13("magnetisch"));
+	}
+	
+	@Test
+	public void testPlainText() {
+		// Alles was zwischen [] steht, wird NICHT dekodiert.
+		assertEquals("nop[def]tuv", Rot13.encodeRot13("abc[def]ghi"));
+	}
+}
